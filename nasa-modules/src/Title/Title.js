319a16552d9bf32e0c89a370d44a372444077ab2
Gegardus <stgegard@gmail.com>
2023-05-02 11:28:00 +0400

    first commit

---
+++ nasa-modules/src/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Title.module.css';
+
+const Title = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>NASA Modules</h1>
+    </div>
+  );
+}
+
+export default Title;
